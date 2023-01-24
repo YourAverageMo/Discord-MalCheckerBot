291e2ea246e0b7124077bc9d29dd39cbbbc00184
Muhammed Yilmaz <yilmaz@email.arizona.edu>
2023-01-24 00:32:48 -0700

    before i try bot trees

--- bot.py
+++ bot.py
@@ -1,30 +1,26 @@
 import os
 
 import discord
+from discord import app_commands
+from discord.ext import commands
 from dotenv import find_dotenv, load_dotenv
 
 import responses
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
+TOKEN = os.getenv("TOKEN")
 
-# intents are required by discord
+# ------------ INTENTS ARE REQUIRED BY DISCORD
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-
-async def send_message(message, user):
-    try:
-        response = responses.handle_response(user)
-        await message.channel.send(response)
-    except Exception as e:
-        print(e)
+# ------------ SETTING UP BOT COMMANDS
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 
 def run_discord_bot():
-    TOKEN = os.getenv("TOKEN")
-
     # start the bot
     @client.event
     async def on_ready():
@@ -33,18 +29,31 @@ def run_discord_bot():
     client.run(TOKEN)
 
 
-# when bot recieves a meg
-@client.event
-async def on_message(message):
-    if message.author == client.user:  #ignore msg from bot
-        return
 
-    username = str(message.author)
-    user_message = str(message.content)
-    channel = str(message.channel)
+# ------------ BOT FUNCTIONS
+async def send_message(message, user):
+    try:
+        response = responses.handle_response(user)
+        await message.channel.send(response)
+    except Exception as e:
+        print(e)
 
-    print(f"{username} said: {user_message} in {channel}")
 
-    # use this line for setting bot commands
-    # if user_message[0] == "?":
-    await send_message(message, username)
+# ------------ WHEN BOT RECEIVES A MSG
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:  #ignore msg from bot
+#         return
+
+#     username = str(message.author)
+#     user_message = str(message.content)
+#     channel = str(message.channel)
+
+#     print(f"{username} said: {user_message} in {channel}")
+#     await send_message(message, username)
+
+
+# ------------ BOT COMMANDS
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
